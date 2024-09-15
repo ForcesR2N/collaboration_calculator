0d815e29580b920515ba8a544fe835c34005441b
Muhammad Choirulanam <mchoiranam@gmail.com>
2024-09-15 23:39:12 +0700

    geometry code

--- lib/bloc/balok_bloc.dart
+++ lib/bloc/balok_bloc.dart
@@ -1,7 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'balok_event.dart';
 import 'balok_state.dart';
-import '../models/balok.dart';
+
+class Balok {
+  final double panjang;
+  final double lebar;
+  final double tinggi;
+
+  Balok({
+    required this.panjang,
+    required this.lebar,
+    required this.tinggi,
+  });
+
+  double volume() => panjang * lebar * tinggi;
+}
 
 class BalokBloc extends Bloc<BalokEvent, BalokState> {
   BalokBloc() : super(BalokInitial()) {
