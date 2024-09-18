56858488eaab21b2332cedb98d6184aae8066cd7
ForcesR2N <muhammadzuhrizal07@gmail.com>
2024-09-18 22:50:38 +0700

    fix overflowed

--- lib/components/my_colors.dart
+++ lib/components/my_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color mainColor = Color(0xFF00AA13);
+  static const Color mainColor = Color.fromRGBO(0, 170, 19, 1);
   static const Color textColor = Color.fromARGB(255, 7, 7, 7);
   static const IconData bluetooth =
       IconData(0xe0e4, fontFamily: 'MaterialIcons');
